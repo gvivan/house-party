36b41df48ab32550ffe66a0bc14f6c662d44512a
gvivan <103859388+gvivan@users.noreply.github.com>
2022-09-05 12:30:11 -0400

    update(): change setup to support material-ui

---
+++ api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'can_guest_pause',
+                  'votes_to_skip', 'created_at')
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('can_guest_pause', 'votes_to_skip')
